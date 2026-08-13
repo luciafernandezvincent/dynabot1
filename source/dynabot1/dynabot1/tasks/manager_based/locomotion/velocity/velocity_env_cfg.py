5c82e62f8153f5bd233f5db8130051fa988bdf89
luciafernandezvincent <lfernandezvincent@udesa.edu.ar>
2026-08-13 17:08:49 -0300

    gaussian errors for imu

--- source/dynabot1/dynabot1/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
+++ source/dynabot1/dynabot1/tasks/manager_based/locomotion/velocity/velocity_env_cfg.py
@@ -128,23 +128,14 @@ class ObservationsCfg:
         """Observations for policy group."""
 
         # observation terms (order preserved)
-        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Unoise(n_min=-0.3, n_max=0.3))
-        base_ang_vel = ObsTerm(
-            func=mdp.base_ang_vel,
-            noise=Gnoise(
-                mean=0.0,
-                std=math.radians(0.015)
-            ),
-        )
+        base_lin_vel = ObsTerm(func=mdp.base_lin_vel, noise=Gnoise(mean=0.0, std=0.02))
+        base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Gnoise(mean=0.0, std= 0.015))
         #base_ang_vel = ObsTerm(func=mdp.base_ang_vel, noise=Unoise(n_min=-0.2, n_max=0.2))
         
-        projected_gravity = ObsTerm(
-            func=mdp.projected_gravity,
-            noise=Unoise(n_min=-0.05, n_max=0.05),
-        )
+        projected_gravity = ObsTerm(func=mdp.projected_gravity,noise=Gnoise(mean=0.0, std=0.048))
         velocity_commands = ObsTerm(func=mdp.generated_commands, params={"command_name": "base_velocity"})
-        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Unoise(n_min=-0.01, n_max=0.01))
-        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Unoise(n_min=-1.5, n_max=1.5))
+        joint_pos = ObsTerm(func=mdp.joint_pos_rel, noise=Gnoise(mean=0.0, std=0.1))
+        joint_vel = ObsTerm(func=mdp.joint_vel_rel, noise=Gnoise(mean=0.0,std=0.1))
         actions = ObsTerm(func=mdp.last_action)
 
         # height_scan = ObsTerm(
